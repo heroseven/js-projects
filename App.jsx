04cf4144f75986e3185cd77d0c7175b7cb846a7f
heroseven <nbr920@gmail.com>
2016-05-16 00:56:53 +0000

    findDOMNode

--- App.jsx
+++ App.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 class App extends React.Component {
    constructor() {
       super();
-		
-      this.state = {
-         data: []
-      }
-	
-      this.setStateHandler = this.setStateHandler.bind(this);
+	   //se le esta pasando logica del construector al metodo findDomNodeHandler
+      this.getDomNodeHandler = this.findDomNodeHandler.bind(this);
    };
 
-   setStateHandler() {
-      var item = "setState..."
-      var myArray = this.state.data;
-      myArray.push(item)
-      this.setState({data: myArray})
-   };
+   findDomNodeHandler(){
+      var myDiv= document.getElementById('myDiv');
+      ReactDOM.findDOMNode(myDiv).style.color='green';
+   }
 
    render() {
       return (
          <div>
-            <button onClick = {this.setStateHandler}>SET STATE</button>
-            <h4>State Array: {this.state.data}</h4>
+            <button onClick={this.findDomNodeHandler}>FIND DOM HANDLER</button>
+            <div id="myDiv">Node</div>
          </div>
       );
    }
