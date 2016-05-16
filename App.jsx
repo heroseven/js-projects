dd68ce849600447fbb373758a6b2f3b0e6a79d88
heroseven <nbr920@gmail.com>
2016-05-16 01:41:29 +0000

    hola

--- App.jsx
+++ App.jsx
@@ -4,55 +4,41 @@ class App extends React.Component {
    constructor() {
       super();
 	   //se le esta pasando logica del construector al metodo findDomNodeHandler
-      this.getDomNodeHandler = this.findDomNodeHandler.bind(this);
+      this.setPlusOne= this.setPlusOne.bind(this);
+      
+      this.state={
+         data:0,
+      }
    };
-
-   findDomNodeHandler(){
-      var myDiv= document.getElementById('myDiv');
-      ReactDOM.findDOMNode(myDiv).style.color='green';
+   
+   setPlusOne(){
+      
+      this.setState({data: this.state.data+1});
    }
+
+  
 
    render() {
       return (
          <div>
-            <button onClick={this.findDomNodeHandler}>FIND DOM HANDLER</button>
-            <div id="myDiv">Node</div>
+            <button onClick={this.setPlusOne}>SUMAR +1</button>
+            <Mostrar myNumber={this.state.data}/>
          </div>
       );
    }
 }
 
-class Header extends React.Component {
+
+class Mostrar extends React.Component {
+ 
    render() {
       return (
          <div>
-            <h1>Header2222</h1>
+            <h3>{this.props.myNumber}</h3>
          </div>
       );
    }
 }
 
-class Component1 extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>{this.props.nombre}</h1>
-            <h2>{this.props.codigo}</h2>
-         </div>
-      );
-   }
-}
-
-class TableRow extends React.Component {
-   render() {
-      return (
-         <tr>
-            <td>{this.props.data.id}</td>
-            <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
-         </tr>
-      );
-   }
-}
 
 export default App;
